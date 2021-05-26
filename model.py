5b052de1b50774c29e5551b09862c963a12ac7c8
aleenajohnsony <40754257+aleenajohnsony@users.noreply.github.com>
2021-05-27 00:39:10 +0530

    Update model.py

--- model.py
+++ model.py
@@ -92,13 +92,92 @@ class Decoder(nn.Module):
 
         return y_coarse, y_detail
 
+class Enc_fold(nn.Module):
+    def __init__(self):
+        super(Enc_fold, self).__init__()
+        self.feat = PointNetfeat(global_feat=True)
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 512)
+        # self.fc2 = nn.Linear(512, 256)
+        # self.fc3 = nn.Linear(256, k)
+        self.bn1 = nn.BatchNorm1d(512)
+        # self.bn2 = nn.BatchNorm1d(256)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x, trans = self.feat(x)  # x = batch,1024
+        x = F.relu(self.bn1(self.fc1(x)))  # x = batch,512
+        x = self.fc2(x)  # x = batch,512
+
+        return x
+class Dec_fold(nn.Module):
+    def __init__(self):
+        super(Dec_fold, self).__init__()
+        self.fold1 = DecFold1()
+        self.fold2 = DecFold2()
+
+    def forward(self, x):  # input x = batch, 512
+        batch_size = x.size(0)
+        x = torch.unsqueeze(x, 1)  # x = batch,1,512
+        x = x.repeat(1, 45 ** 2, 1)  # x = batch,45^2,512
+        code = x
+        code = x.transpose(2, 1)  # x = batch,512,45^2
+
+        meshgrid = [[-0.3, 0.3, 45], [-0.3, 0.3, 45]]
+        grid = GridSamplingLayer(batch_size, meshgrid)  # grid = batch,45^2,2
+        grid = torch.from_numpy(grid)
+
+        if x.is_cuda:
+            grid = grid.cuda()
+
+        x = torch.cat((x, grid), 2)  # x = batch,45^2,514
+        x = x.transpose(2, 1)  # x = batch,514,45^2
+
+        x = self.fold1(x)  # x = batch,3,45^2
+        p1 = x  # to observe
+
+        x = torch.cat((code, x), 1)  # x = batch,515,45^2
+
+        x = self.fold2(x)  # x = batch,3,45^2
+
+        return x
+class DecFold1(nn.Module):
+    def __init__(self):
+        super(DecFold1, self).__init__()
+        self.conv1 = nn.Conv1d(514, 512, 1)
+        self.conv2 = nn.Conv1d(512, 512, 1)
+        self.conv3 = nn.Conv1d(512, 3, 1)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x):  # input x = batch,514,45^2
+        x = self.relu(self.conv1(x))  # x = batch,512,45^2
+        x = self.relu(self.conv2(x))
+        x = self.conv3(x)
+
+        return x
+
+
+class DecFold2(nn.Module):
+    def __init__(self):
+        super(DecFold2, self).__init__()
+        self.conv1 = nn.Conv1d(515, 512, 1)
+        self.conv2 = nn.Conv1d(512, 512, 1)
+        self.conv3 = nn.Conv1d(512, 3, 1)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):  # input x = batch,515,45^2
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.conv3(x)
+        return x
 
 class AutoEncoder(nn.Module):
     def __init__(self):
         super(AutoEncoder, self).__init__()
 
-        self.encoder = Encoder()
-        self.decoder = Decoder()
+        self.encoder = Enc_fold()
+        self.decoder = Dec_fold()
     
     def forward(self, x):
         v = self.encoder(x)
