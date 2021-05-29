3c2e7c9186586346a2af0cd13414eeec57c5e3be
LebaAnna <37035966+LebaAnna@users.noreply.github.com>
2021-05-29 17:28:22 +0530

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -21,7 +21,7 @@ test_dataset = ShapeNet(partial_path=args.partial_root, gt_path=args.gt_root, sp
 test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
 
 network = AutoEncoder()
-network.load_state_dict(torch.load('log/lowest_loss.pth'))
+network.load_state_dict(torch.load('/dataset/test/trained_model_pcn.pth'))
 network.to(DEVICE)
 
 # testing: evaluate the mean cd loss
