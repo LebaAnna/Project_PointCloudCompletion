991a9f2090f7d097f5d419603f604bc6e3adc206
aleenajohnsony <40754257+aleenajohnsony@users.noreply.github.com>
2021-06-09 18:53:53 +0530

    Update model.py

--- model.py
+++ model.py
@@ -5,109 +5,134 @@ import torch.nn.functional as F
 
 
 class Encoder(nn.Module):
-    def __init__(self, num_points):
+    def __init__(self):
         super(Encoder, self).__init__()
-        self.num_points = num_points
-        self.conv1 = nn.Conv1d(3, 64, 1)
-        self.conv2 = nn.Conv1d(64, 128, 1)
-        self.conv3 = nn.Conv1d(128, 1024, 1)
-        self.bn1 = nn.BatchNorm1d(64)
-        self.bn2 = nn.BatchNorm1d(128)
-        self.bn3 = nn.BatchNorm1d(1024)
 
-        self.fc1 = nn.Linear(1024 + 64, 1024)
-        self.fc2 = nn.Linear(1024, 512)  
+        # first shared mlp
+        self.conv1 = nn.Conv1d(3, 128, 1)
+        self.conv2 = nn.Conv1d(128, 256, 1)
+        self.bn1 = nn.BatchNorm1d(128)
+        self.bn2 = nn.BatchNorm1d(256)
+
+        # second shared mlp
+        self.conv3 = nn.Conv1d(512, 512, 1)
+        self.conv4 = nn.Conv1d(512, 1024, 1)
+        self.bn3 = nn.BatchNorm1d(512)
+        self.bn4 = nn.BatchNorm1d(1024)
+    
+    def forward(self, x):
+        n = x.size()[2]
+
+        # first shared mlp
+        x = F.relu(self.bn1(self.conv1(x)))           # (B, 128, N)
+        f = self.bn2(self.conv2(x))                   # (B, 256, N)
         
-
-    def forward(self, input):
-        input = input.transpose(2, 1)
-        x = F.relu(self.bn1(self.conv1(input)))
-        local_feature = x  
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        x = torch.max(x, 2, keepdim=True)[0]
-        global_feature = x.view(-1, 1024, 1).repeat(1, 1, self.num_points)
-        feature = torch.cat([local_feature, global_feature], 1)  
-
+        # point-wise maxpool
+        g = torch.max(f, dim=2, keepdim=True)[0]      # (B, 256, 1)
         
-        x = F.relu(self.fc1(feature.transpose(1, 2)))
-        x = F.relu(self.fc2(x))
+        # expand and concat
+        x = torch.cat([g.repeat(1, 1, n), f], dim=1)  # (B, 512, N)
 
-        return torch.max(x, 1, keepdim=True)[0]  
+        # second shared mlp
+        x = F.relu(self.bn3(self.conv3(x)))           # (B, 512, N)
+        x = self.bn4(self.conv4(x))                   # (B, 1024, N)
+        
+        # point-wise maxpool
+        v = torch.max(x, dim=-1)[0]                   # (B, 1024)
+        
+        return v
 
 
 class Decoder(nn.Module):
-    def __init__(self, num_points=2048, m=2025):
+    def __init__(self, num_coarse=1024, num_dense=16384):
         super(Decoder, self).__init__()
-        self.n = num_points  
-        self.m = m  
-        self.meshgrid = [[-0.3, 0.3, 45], [-0.3, 0.3, 45]]
-        self.mlp1 = nn.Sequential(
-            nn.Conv1d(514, 256, 1),
-            nn.ReLU(),
-            nn.Conv1d(256, 64, 1),
-            nn.ReLU(),
-            nn.Conv1d(64, 3, 1),
-            
-        )
 
-        self.mlp2 = nn.Sequential(
-            nn.Conv1d(515, 256, 1),
-            nn.ReLU(),
-            nn.Conv1d(256, 64, 1),
-            nn.ReLU(),
-            nn.Conv1d(64, 3, 1),
-           
-        )
+        self.num_coarse = num_coarse
+        
+        # fully connected layers
+        self.linear1 = nn.Linear(1024, 1024)
+        self.linear2 = nn.Linear(1024, 1024)
+        self.linear3 = nn.Linear(1024, 3 * num_coarse)
+        self.bn1 = nn.BatchNorm1d(1024)
+        self.bn2 = nn.BatchNorm1d(1024)
 
-    def build_grid(self, batch_size):
-        x = np.linspace(*self.meshgrid[0])
-        y = np.linspace(*self.meshgrid[1])
-        grid = np.array(list(itertools.product(x, y)))
-        grid = np.repeat(grid[np.newaxis, ...], repeats=batch_size, axis=0)
-        grid = torch.tensor(grid)
-        return grid.float()
+        # shared mlp 1
+        self.conv1 = nn.Conv1d(3+2+1024, 512, 1)
+        self.conv2 = nn.Conv1d(512, 512, 1)
+        self.conv3 = nn.Conv1d(512, 3, 1)
+        self.bn3 = nn.BatchNorm1d(512)
+        self.bn4 = nn.BatchNorm1d(512)
+        
+        # shared mlp 2
+        self.conv4 = nn.Conv1d(3+2+1024, 512, 1)
+        self.conv5 = nn.Conv1d(512, 512, 1)
+        self.conv6 = nn.Conv1d(512, 3, 1)
+        self.bn5 = nn.BatchNorm1d(512)
+        self.bn6 = nn.BatchNorm1d(512)
 
-    def forward(self, input):
-        input = input.transpose(1, 2).repeat(1, 1, self.m)  
-        grid = self.build_grid(input.shape[0]).transpose(1, 2)  
-        if torch.cuda.is_available():
-            grid = grid.cuda()
-        concate1 = torch.cat((input, grid), dim=1) 
-        after_folding1 = self.mlp1(concate1) 
-        concate2 = torch.cat((input, after_folding1), dim=1)  
-        after_folding2 = self.mlp2(concate2)  
-        return after_folding2.transpose(1, 2)  
+        # 2D grid
+        grids = np.meshgrid(np.linspace(-0.05, 0.05, 4, dtype=np.float32),
+                            np.linspace(-0.05, 0.05, 4, dtype=np.float32))                               # (2, 4, 44)
+        self.grids = torch.Tensor(grids).view(2, -1)  # (2, 4, 4) -> (2, 16)
+    
+    def forward(self, x):
+        b = x.size()[0]
+        # global features
+        v = x  # (B, 1024)
+
+        # fully connected layers to generate the coarse output
+        x = F.relu(self.bn1(self.linear1(x)))
+        x = F.relu(self.bn2(self.linear2(x)))
+        x = self.linear3(x)
+        y_coarse = x.view(-1, 3, self.num_coarse)  # (B, 3, 1024)
+
+        repeated_centers = y_coarse.unsqueeze(3).repeat(1, 1, 1, 16).view(b, 3, -1)  # (B, 3, 16x1024)
+        repeated_v = v.unsqueeze(2).repeat(1, 1, 16 * self.num_coarse)               # (B, 1024, 16x1024)
+        grids = self.grids.to(x.device)  # (2, 16)
+        grids = grids.unsqueeze(0).repeat(b, 1, self.num_coarse)                     # (B, 2, 16x1024)
+        
+        #folding 1
+        x = torch.cat([repeated_v, grids, repeated_centers], dim=1)                  # (B, 2+3+1024, 16x1024)
+        x = F.relu(self.bn3(self.conv1(x)))
+        x = F.relu(self.bn4(self.conv2(x)))
+        x = self.conv3(x)                # (B, 3, 16x1024)
+        
+        #folding 2
+        x = torch.cat([repeated_v, x, repeated_centers], dim=1)                 
+        x = F.relu(self.bn5(self.conv4(x)))
+        x = F.relu(self.bn6(self.conv5(x)))
+        x = self.conv6(x) 
+        y_detail = x + repeated_centers  # (B, 3, 16x1024)
+
+        return y_coarse, y_detail
 
 
 class Folding(nn.Module):
-    def __init__(self, num_points):
+    def __init__(self):
         super(Folding, self).__init__()
 
-        self.encoder = Encoder(num_points=num_points)
-        self.decoder = Decoder(num_points=num_points)
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+    
+    def forward(self, x):
+        v = self.encoder(x)
+        y_coarse, y_detail = self.decoder(v)
+        return v, y_coarse, y_detail
 
-    def forward(self, input):
-        codeword = self.encoder(input)
-        output = self.decoder(codeword)
-        return output
-
-    def get_parameter(self):
-        return list(self.encoder.parameters()) + list(self.decoder.parameters())
-
-    def get_loss(self, input, output):
-        return self.loss(input, output)
 
 if __name__ == "__main__":
     pcs = torch.rand(16, 3, 2048)
     encoder = Encoder()
     v = encoder(pcs)
-
+    print(v.size())
 
     decoder = Decoder()
     decoder(v)
     y_c, y_d = decoder(v)
+    print(y_c.size(), y_d.size())
 
     ae = Folding()
     v, y_coarse, y_detail = ae(pcs)
+    print(v.size(), y_coarse.size(), y_detail.size())
+    
    
